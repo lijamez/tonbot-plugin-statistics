a07c184dd1c582d0f73ebd5b6a4815f9e117c63b
James Li <lijamez@users.noreply.github.com>
2017-10-15 01:08:14 -0700

    Compatibility with latest PeriodicTask API.

--- src/main/java/net/tonbot/plugin/statistics/StatisticsLogger.java
+++ src/main/java/net/tonbot/plugin/statistics/StatisticsLogger.java
@@ -3,6 +3,8 @@ package net.tonbot.plugin.statistics;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import com.google.common.base.Preconditions;
+
 import net.tonbot.common.PeriodicTask;
 import sx.blah.discord.api.IDiscordClient;
 
@@ -10,13 +12,16 @@ class StatisticsLogger extends PeriodicTask {
 
 	private static final Logger LOG = LoggerFactory.getLogger(StatisticsLogger.class);
 
+	private final IDiscordClient discordClient;
+	
 	public StatisticsLogger(IDiscordClient discordClient, long periodMs) {
-		super(discordClient, periodMs);
+		super(periodMs);
+		
+		this.discordClient = Preconditions.checkNotNull(discordClient, "discordClient must be non-null.");
 	}
 
 	@Override
 	public void performTask() {
-		IDiscordClient discordClient = this.getDiscordClient();
 		LOG.info("Currently in " + discordClient.getGuilds().size() + " guilds.");
 		LOG.info("Is ready? " + discordClient.isReady());
 		LOG.info("Is logged in? " + discordClient.isLoggedIn());
